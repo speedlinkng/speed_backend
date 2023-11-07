53af31e05b4863caa9fd10863a6687ee655cebb0
drrowly99 <drrowly99@gmail.com>
2023-11-07 06:12:18 +0100

    send backend

--- src/models/DB.js
+++ src/models/DB.js
@@ -6,7 +6,7 @@ const pool = createPool({
     port : process.env.DB_PORT,
     host : process.env.DB_HOST,
     user : process.env.DB_USERNAME,
-    password : process.env.DB_PASSWORD,
+    password : '',
     database : process.env.DB_DATABASE,
     connectonLimit : 10
 });
