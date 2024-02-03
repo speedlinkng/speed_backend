1421e76af09aa5b8597dcc52281f48753866dc40
drrowly99 <drrowly99@gmail.com>
2024-02-03 12:02:56 -0800

    pushing updates to online after a long break

--- src/models/PGDB.js
+++ src/models/PGDB.js
@@ -2,10 +2,10 @@ const { Pool } = require('pg');
 
 // Create a PostgreSQL connection pool
 const pool = new Pool({
-  user: 'postgres',
-  host: 'localhost',
-  database: 'speedlink',
-  password: '123456',
+  user: 'root',
+  host: 'dpg-cmv9lsgcmk4c73agjpbg-a',
+  database: 'speed_db',
+  password: 'qC4mpRC438WE5rGgQVUZvM7KzsUr3KTw',
   port: 5432,
 });
 
