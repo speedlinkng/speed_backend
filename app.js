21e6042c735b1316cf728740677584b670f9f341
drrowly99 <drrowly99@gmail.com>
2024-04-05 09:43:38 -0700

    fix

--- app.js
+++ app.js
@@ -111,7 +111,7 @@ app.get('/', function(req, res) {
         return client;
       } catch (error) {
         console.error('Error acquiring client from the pool', error);
-        res.send('error was received');
+        res.send('error was received'+error);
         throw error;
       }
     }
