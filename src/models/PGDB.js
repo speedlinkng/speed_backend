d25293aeedeba09929b9eaf9f26b3f60bfe91453
drrowly99 <drrowly99@gmail.com>
2024-05-13 11:02:13 -0700

    DB UPDATE TO RENDER

--- src/models/PGDB.js
+++ src/models/PGDB.js
@@ -1,13 +1,13 @@
 const { Pool } = require('pg');
 
 // Create a PostgreSQL connection pool
-// const pool = new Pool({
-//   user: 'postgres',
-//   host: 'localhost',
-//   database: 'speedlink',
-//   password: '123456',
-//   port: 5432,
-// });
+const pool = new Pool({
+  user: 'postgres',
+  host: 'localhost',
+  database: 'speedlink',
+  password: '123456',
+  port: 5432,
+});
 
 // Connect to Render
 const pool = new Pool({
