5ba79c5e5d3b91efaea27bf7bfa466e05b457b33
drrowly99 <drrowly99@gmail.com>
2024-05-13 11:02:30 -0700

    DB UPDATE TO RENDER

--- src/models/PGDB.js
+++ src/models/PGDB.js
@@ -10,13 +10,13 @@ const pool = new Pool({
 });
 
 // Connect to Render
-const pool = new Pool({
-  user: process.env.DB_USERNAME,
-  host: process.env.DB_HOST,
-  database: process.env.DB_DATABASE,
-  password: process.env.DB_PASSWORD,
-  port: 5432,
-});
+// const pool = new Pool({
+//   user: process.env.DB_USERNAME,
+//   host: process.env.DB_HOST,
+//   database: process.env.DB_DATABASE,
+//   password: process.env.DB_PASSWORD,
+//   port: 5432,
+// });
 
 
 // const pool = new Pool({
