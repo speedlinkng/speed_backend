1d5a788277931386ef6e0a45e272e8bee5211e9c
drrowly99 <drrowly99@gmail.com>
2024-05-14 05:51:43 -0700

    DB ADJUSTMENTS MADE TO REFLECT ON PRODUCTION OROTHERWISE

--- src/models/PGDB.js
+++ src/models/PGDB.js
@@ -1,17 +1,35 @@
 const { Pool } = require('pg');
 const dotenv = require('dotenv')
 dotenv.config();
+
+
 // Create a PostgreSQL connection pool
-const pool = new Pool({
-  user: 'postgres',
-  host: 'localhost',
-  database: 'speedlink',
-  password: '123456',
-  port: 5432,
-  ssl: {
+var pool
+if (process.env.NODE_ENV === 'production') {
+ pool = new Pool({
+  user: process.env.DB_USERNAME,
+  host: process.env.DB_HOST,
+  database: process.env.DB_DATABASE,
+  password: process.env.DB_PASSWORD,
+   port: 5432,
+   ssl: {
     rejectUnauthorized: false // Set this to true if you have a valid SSL certificate
   }
 });
+
+} else {
+  pool = new Pool({
+    user: 'postgres',
+    host: 'localhost',
+    database: 'speedlink',
+    password: '123456',
+    port: 5432,
+    ssl: {
+      rejectUnauthorized: false // Set this to true if you have a valid SSL certificate
+    }
+  });
+}
+
 
 // Connect to Render
 // const pool = new Pool({
